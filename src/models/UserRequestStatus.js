c2f453c94abd27e7b2dbc89f27db3529e1262be1
Darlan Tódero ten Caten <darlan.caten@simplyas.com>
2021-05-01 13:42:27 -0300

    feat: added UserRequestStatus model

---
+++ src/models/UserRequestStatus.js
@@ -0,0 +1,8 @@
+const UserRequestStatus = Object.freeze({
+  waitingForApproval: "waitingForApproval",
+  available: "available",
+  canceled: "canceled",
+  paid: "paid",
+});
+
+export default UserRequestStatus;
