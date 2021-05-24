b896988cde33bc5ca13e79d9418ee57e44a330b9
Darlan Tódero ten Caten <darlan.caten@simplyas.com>
2021-05-24 11:26:17 -0300

    feat: added UserRequestModel

---
+++ src/models/UserRequestModel.js
@@ -0,0 +1,26 @@
+class UserRequestModel {
+    createdAt;
+    description;
+    userId;
+    pixKey;
+    status;
+
+    constructor(data) {
+        if (
+            data &&
+            data.hasOwnProperty("createdAt") &&
+            data.hasOwnProperty("userId") &&
+            data.hasOwnProperty("pixKey")
+        ) {
+            this.createdAt = data.createdAt;
+            this.description = data.description;
+            this.userId = data.userId;
+            this.pixKey = data.pixKey;
+            this.status = data.status;
+        } else {
+            throw new Error("Invalid data");
+        }
+    }
+}
+
+export default UserRequestModel;
