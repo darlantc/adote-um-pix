0da195c28718653e0bdd0f8f19a99e3ec63ce268
Darlan Tódero ten Caten <darlan.caten@simplyas.com>
2021-05-25 13:48:24 -0300

    Fix: removed wrong static methods

--- src/models/builders/UserRequestBuilder.js
+++ src/models/builders/UserRequestBuilder.js
@@ -4,49 +4,49 @@ import UserRequestModel from "../UserRequestModel";
 import UserRequestStatus from "../UserRequestStatus";
 
 class UserRequestBuilder {
-  model = new UserRequestModel({
-    id: uuid(),
-    createdAt: new Date().getTime(),
-    userId: uuid(),
-    pixKey: uuid(),
-    status: UserRequestStatus.waitingForApproval,
-  });
+    model = new UserRequestModel({
+        id: uuid(),
+        createdAt: new Date().getTime(),
+        userId: uuid(),
+        pixKey: uuid(),
+        status: UserRequestStatus.waitingForApproval,
+    });
 
-  static aUserRequest() {
-    return new UserRequestBuilder();
-  }
+    static aUserRequest() {
+        return new UserRequestBuilder();
+    }
 
-  static withCustomDescription(description) {
-    this.model.description = description;
-    return this;
-  }
+    withCustomDescription(description) {
+        this.model.description = description;
+        return this;
+    }
 
-  static withCustomPixKey(pixKey) {
-    this.model.pixKey = pixKey;
-    return this;
-  }
+    withCustomPixKey(pixKey) {
+        this.model.pixKey = pixKey;
+        return this;
+    }
 
-  static withCustomId(id) {
-    this.model.id = id;
-    return this;
-  }
+    withCustomId(id) {
+        this.model.id = id;
+        return this;
+    }
 
-  static withCanceledStatus() {
-    this.model.status = UserRequestStatus.canceled;
-    return this;
-  }
-  static withAvailableStatus() {
-    this.model.status = UserRequestStatus.available;
-    return this;
-  }
-  static withPaidStatus() {
-    this.model.status = UserRequestStatus.paid;
-    return this;
-  }
+    withCanceledStatus() {
+        this.model.status = UserRequestStatus.canceled;
+        return this;
+    }
+    withAvailableStatus() {
+        this.model.status = UserRequestStatus.available;
+        return this;
+    }
+    withPaidStatus() {
+        this.model.status = UserRequestStatus.paid;
+        return this;
+    }
 
-  build() {
-    return this.model;
-  }
+    build() {
+        return this.model;
+    }
 }
 
 export default UserRequestBuilder;
