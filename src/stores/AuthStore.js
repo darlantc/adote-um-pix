2d347aea11dcaefff1e3eebe24fabca1fe98fa2f
Darlan Tódero ten Caten <darlan.caten@simplyas.com>
2021-05-31 11:33:01 -0300

    feat: temporary code to fake user

--- src/stores/AuthStore.js
+++ src/stores/AuthStore.js
@@ -3,78 +3,77 @@ import { makeObservable, observable, action } from "mobx";
 import LoginStatus from "../models/LoginStatus";
 
 class AuthStore {
-  loggedUser = null;
-  loginStatus = LoginStatus.offline;
-  error = null;
+    loggedUser = null;
+    loginStatus = LoginStatus.offline;
+    error = null;
 
-  constructor(firebaseService) {
-    this.firebaseService = firebaseService;
-    makeObservable(this, {
-      loggedUser: observable,
-      loginStatus: observable,
-      setLoggedUser: action,
-      setLoginStatus: action,
-    });
-    this.confirmEmailSignIn();
-  }
+    constructor(firebaseService) {
+        this.firebaseService = firebaseService;
+        makeObservable(this, {
+            loggedUser: observable,
+            loginStatus: observable,
+            setLoggedUser: action,
+            setLoginStatus: action,
+        });
+        // this.confirmEmailSignIn();
 
-  setLoggedUser = (value) => {
-    this.loggedUser = value;
-  };
+        // TODO: Código temporário pra validar essa branch
+        this.setLoggedUser({
+            uid: "user1",
+        });
+    }
 
-  setLoginStatus = (value) => {
-    this.loginStatus = value;
-  };
-
-  setError = (value) => {
-    this.error = value;
-  };
-
-  configSignInEmail = () => {
-    return {
-      url: "http://localhost:3000/",
-      handleCodeInApp: true,
+    setLoggedUser = (value) => {
+        this.loggedUser = value;
     };
-  };
 
-  sendSignInLinkToEmail = async (email) => {
-    this.setLoginStatus(LoginStatus.loading);
-    try {
-      await this.firebaseService.auth.sendSignInLinkToEmail(
-        email,
-        this.configSignInEmail()
-      );
+    setLoginStatus = (value) => {
+        this.loginStatus = value;
+    };
 
-      localStorage.setItem("emailForSignIn", email);
+    setError = (value) => {
+        this.error = value;
+    };
 
-      this.setLoginStatus(LoginStatus.online);
-    } catch (error) {
-      this.setError(error);
-    }
-  };
+    configSignInEmail = () => {
+        return {
+            url: "http://localhost:3000/",
+            handleCodeInApp: true,
+        };
+    };
 
-  confirmEmailSignIn = async () => {
-    const ref = window.location.href;
-    if (this.firebaseService.auth.isSignInWithEmailLink(ref)) {
-      let email = window.localStorage.getItem("emailForSignIn");
-      if (!email) {
-        email = window.prompt("Please provide your email for confirmation");
-      }
+    sendSignInLinkToEmail = async (email) => {
+        this.setLoginStatus(LoginStatus.loading);
+        try {
+            await this.firebaseService.auth.sendSignInLinkToEmail(email, this.configSignInEmail());
 
-      try {
-        const result = await this.firebaseService.auth.signInWithEmailLink(
-          email,
-          ref
-        );
+            localStorage.setItem("emailForSignIn", email);
 
-        window.localStorage.removeItem("emailForSignIn");
-        this.setLoggedUser(result.user);
-        this.setLoginStatus(LoginStatus.online);
-      } catch (error) {
-        this.setError(error);
-      }
-    }
-  };
+            this.setLoginStatus(LoginStatus.online);
+        } catch (error) {
+            this.setError(error);
+        }
+    };
+
+    confirmEmailSignIn = async () => {
+        const ref = window.location.href;
+        if (this.firebaseService.auth.isSignInWithEmailLink(ref)) {
+            let email = window.localStorage.getItem("emailForSignIn");
+            if (!email) {
+                email = window.prompt("Please provide your email for confirmation");
+            }
+
+            try {
+                const result = await this.firebaseService.auth.signInWithEmailLink(email, ref);
+
+                window.localStorage.removeItem("emailForSignIn");
+                this.setLoggedUser(result.user);
+                this.setLoginStatus(LoginStatus.online);
+            } catch (error) {
+                this.setError(error);
+            }
+        }
+    };
 }
 
 export default AuthStore;
