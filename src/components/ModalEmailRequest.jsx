0a653e5ae9c915e2f86a57fa02810404424077de
Mateus Pereira <mateusp.s@outlook.com>
2021-06-08 15:08:08 -0300

    refactor: resized Box

--- src/components/ModalEmailRequest.jsx
+++ src/components/ModalEmailRequest.jsx
@@ -28,11 +28,27 @@ const ModalEmailRequest = observer(() => {
     return (
         <Modal open={emailForSignIn}>
             <Box display="flex" justifyContent="center">
-                <Box borderRadius={7} position="absolute" top="15vh" bgcolor="background.paper" padding="10px">
+                <Box
+                    borderRadius={7}
+                    position="absolute"
+                    top="15vh"
+                    bgcolor="background.paper"
+                    padding="10px"
+                    width="50%"
+                >
                     <Typography variant="h6">Confirme o seu email:</Typography>
-                    <TextField value={email} onChange={(event) => setEmail(event.target.value)} fullWidth required />
-                    <Box m={1} display="flex" justifyContent="center">
-                        <Button variant="outlined" size="medium" onClick={didConfirmEmail}>
+                    <TextField
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                        fullWidth
+                        required
+                    />
+                    <Box m={2} display="flex" justifyContent="center">
+                        <Button
+                            variant="outlined"
+                            size="medium"
+                            onClick={didConfirmEmail}
+                        >
                             Confirmar
                         </Button>
                     </Box>
