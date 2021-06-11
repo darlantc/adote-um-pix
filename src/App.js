74be2569f746193108d08fabec7a749d7f9ea521
Darlan Tódero ten Caten <darlan.caten@simplyas.com>
2021-06-11 11:45:11 -0300

    feat: moved theme for its own file on jss/styles

--- src/App.js
+++ src/App.js
@@ -1,47 +1,38 @@
 import { BrowserRouter } from "react-router-dom";
 import { Container } from "@material-ui/core";
-import {
-  createMuiTheme,
-  ThemeProvider,
-  makeStyles,
-} from "@material-ui/core/styles";
+import { ThemeProvider, makeStyles } from "@material-ui/core/styles";
 
 import PixBackground from "./assets/images/pix-background.png";
 import StyledAppBar from "./components/StyledAppBar";
 import Routes from "./routes/Routes";
-
-const theme = createMuiTheme({
-  typography: {
-    fontFamily: "Original Surfer",
-  },
-});
+import { theme } from "./assets/jss/styles.js";
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    padding: theme.spacing(4, 2, 4),
-    maxWidth: "700px",
-    backgroundImage: `url(${PixBackground})`,
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center center",
-    backgroundSize: "cover",
-    backgroundAttachment: "fixed",
-    height: "100%",
-  },
+    container: {
+        padding: theme.spacing(4, 2, 4),
+        maxWidth: "700px",
+        backgroundImage: `url(${PixBackground})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center center",
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed",
+        height: "100%",
+    },
 }));
 
 const App = () => {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <StyledAppBar />
-        <Container className={classes.container}>
-          <Routes />
-        </Container>
-      </ThemeProvider>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <ThemeProvider theme={theme}>
+                <StyledAppBar />
+                <Container className={classes.container}>
+                    <Routes />
+                </Container>
+            </ThemeProvider>
+        </BrowserRouter>
+    );
 };
 
 export default App;
