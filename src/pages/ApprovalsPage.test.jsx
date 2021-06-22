82546e929bbc5cb4608e72848d8e9272e881ba9c
Darlan Tódero ten Caten <darlan.caten@simplyas.com>
2021-06-22 11:27:33 -0300

    test: updated ApprovalsPage with Builder

--- src/pages/ApprovalsPage.test.jsx
+++ src/pages/ApprovalsPage.test.jsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import { render } from "@testing-library/react";
 import ApprovalsPage from "./ApprovalsPage";
 import { theme } from "../assets/jss/styles.js";
+import UserRequestBuilder from "../models/builders/UserRequestBuilder";
 
 describe("<ApprovalsPage />", () => {
     it("should not present if requestsList is undefined", () => {
@@ -9,27 +10,32 @@ describe("<ApprovalsPage />", () => {
         expect(queryByLabelText("card")).not.toBeInTheDocument();
     });
 
+    it("should present message when the requestsList is empty", () => {
+        const { queryByText } = getRenderer({ requestsList: [] });
+        expect(queryByText("Não existe nada para avaliar no momento.")).toBeInTheDocument();
+    });
+
     it("should have a Card", () => {
-        const { getByLabelText } = getRenderer({ requestsList: [] });
+        const { getByLabelText } = getRenderer({ requestsList: [createRequest()] });
         expect(getByLabelText("card")).toBeInTheDocument();
     });
 
     it.each(["Nome da pessoa", "1/97", "E-mail/Telefone", "Bio:", "Descrição:", "Chave Pix:"])(
         "should have label '%s'",
         (expected) => {
-            const { getByText } = getRenderer({ requestsList: [] });
+            const { getByText } = getRenderer({ requestsList: [createRequest()] });
             expect(getByText(expected)).toBeInTheDocument();
         }
     );
 
     it.each(["Aprovar", "Recusar"])("should have a button with label '%s'", (expected) => {
-        const { getByRole } = getRenderer({ requestsList: [] });
+        const { getByRole } = getRenderer({ requestsList: [createRequest()] });
         expect(getByRole("button", { name: expected })).toBeInTheDocument();
     });
 
     it("should allow user to reject", () => {
         const onReject = jest.fn();
-        const { getByRole } = getRenderer({ requestsList: [], onReject });
+        const { getByRole } = getRenderer({ requestsList: [createRequest()], onReject });
 
         expect(onReject).not.toBeCalled();
 
@@ -41,7 +47,7 @@ describe("<ApprovalsPage />", () => {
 
     it("should allow user to approve", () => {
         const onApprove = jest.fn();
-        const { getByRole } = getRenderer({ requestsList: [], onApprove });
+        const { getByRole } = getRenderer({ requestsList: [createRequest()], onApprove });
 
         expect(onApprove).not.toBeCalled();
 
@@ -54,10 +60,15 @@ describe("<ApprovalsPage />", () => {
     // TODO: Escrever testes para os botões de avançar / voltar
 });
 
+// Helpers
 function getRenderer({ requestsList, onReject, onApprove }) {
     return render(
         <ThemeProvider theme={theme}>
             <ApprovalsPage requestsList={requestsList} onReject={onReject} onApprove={onApprove} />
         </ThemeProvider>
     );
+}
+
+function createRequest() {
+    return UserRequestBuilder.aUserRequest().build();
 }
