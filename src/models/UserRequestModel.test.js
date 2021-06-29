978a1907f1f5dcbe5118708407e1f4a7f8565c22
Darlan Tódero ten Caten <darlan.caten@simplyas.com>
2021-06-29 10:40:22 -0300

    feat: changed userId by user on UserRequestModel

--- src/models/UserRequestModel.test.js
+++ src/models/UserRequestModel.test.js
@@ -6,10 +6,19 @@ describe("UserRequestModel", () => {
     it("Should throw error if invalid data", () => {
         expect(() => makeSUT()).toThrow("Invalid data");
         expect(() => makeSUT({})).toThrow("Invalid data");
+        expect(() => makeSUT({ id: uuid() })).toThrow("Invalid data");
         expect(() => makeSUT({ createdAt: 1619887317208 })).toThrow("Invalid data");
-        expect(() => makeSUT({ createdAt: 1619887317208, userId: "48914" })).toThrow("Invalid data");
+        expect(() => makeSUT({ createdAt: 1619887317208, user: { id: uuid() } })).toThrow("Invalid data");
         expect(() =>
-            makeSUT({ createdAt: 1619887317208, userId: "48914", pixKey: "f7de57ab-ee77-4493-aabc-f819f6b1d0b4" })
+            makeSUT({
+                id: uuid(),
+                createdAt: 1619887317208,
+                user: { id: uuid() },
+                pixKey: "f7de57ab-ee77-4493-aabc-f819f6b1d0b4",
+            })
+        ).toThrow("Invalid data");
+        expect(() =>
+            makeSUT({ createdAt: 1619887317208, user: { id: uuid() }, pixKey: "f7de57ab-ee77-4493-aabc-f819f6b1d0b4" })
         ).toThrow("Invalid data");
     });
 
@@ -19,7 +28,9 @@ describe("UserRequestModel", () => {
                 id: uuid(),
                 createdAt: 1619887317208,
                 description: "lorem ipsum dolor asit amet",
-                userId: "48914",
+                user: {
+                    id: uuid(),
+                },
                 pixKey: "f7de57ab-ee77-4493-aabc-f819f6b1d0b4",
                 status: "waitingForApproval",
             })
